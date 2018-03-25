aabbaa44589dd005a77d3f391f1076633e3e188a
JITHIN <jithinek96@gmail.com>
2018-03-25 17:48:57 +0530

    New features

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
+function aBigfunction(){
+console.log('Writing a lot of code.');
+console.log('new features');
+}
+
 function something(){
 console.log('I\'am somthing');
 }
 
 console.log('Hello world!');
 something();
+aBigfunction();
